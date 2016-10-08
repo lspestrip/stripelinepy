e60d956b7458225b495a2e762cd2fe4f2cd0d4fc
Maurizio Tomasi <ziotom78@gmail.com>
2016-10-08 07:51:07 +0200

    Force the code to explicitly use NumPy

--- src/stripsim.py
+++ src/stripsim.py
@@ -5,7 +5,7 @@ import numpy as np
 from fortran_routines import test
 
 def main():
-    print(test([1.0, 2.0, 3.0]))
+    print(test(np.array([1.0, 2.0, 3.0])))
 
 if __name__ == '__main__':
     main()
