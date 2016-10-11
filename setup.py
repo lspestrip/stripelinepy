a611f79007f04f945c1fb91716bc054d44f706da
Maurizio Tomasi <ziotom78@gmail.com>
2016-10-11 15:36:23 +0200

    Update paths in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
      
 wrapper = Extension('fortran_routines',
-                    sources=['src/fortran_routines.f90'],
+                    sources=['stripeline/fortran_routines.f90'],
                     extra_f90_compile_args=["-std=f2003"])
 setup(name='stripsim',
       version='0.1',
@@ -18,7 +18,7 @@ setup(name='stripsim',
       license='MIT',
       url='https://github.com/ziotom78/stripsim',
       long_description=read('README.md'),
-      py_modules=['src/stripsim'],
+      py_modules=['stripeline/stripsim'],
       install_requires=['healpy', 'pyyaml'],
       ext_modules=[wrapper]
 )
