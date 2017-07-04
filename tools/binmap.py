8e55a483ae88dce290a5482805226092528f5d66
Silvia Caprioli <silvia.caprioli@unimi.it>
2017-07-04 16:51:57 +0200

    write I, Q, U and hits maps

--- tools/binmap.py
+++ tools/binmap.py
@@ -15,11 +15,12 @@ def main(tod_filename, nside, map_filename):
         x) for x in ('THETA', 'PHI', 'DETQ1', 'DETQ2', 'DETU1', 'DETU2')]
     pixidx = healpy.ang2pix(nside, theta, phi)
     num_of_pixels = healpy.nside2npix(nside)
-    Imap = binned_map(detQ1 + detQ2 + detU1 + detU2, pixidx, num_of_pixels)
-    Qmap = binned_map(2 * (detQ1 - detQ2), pixidx, num_of_pixels)
-    Umap = binned_map(2 * (detU1 - detU2), pixidx, num_of_pixels)
+    Imap, hits = binned_map(detQ1 + detQ2 + detU1 +
+                            detU2, pixidx, num_of_pixels)
+    Qmap, hits = binned_map(2 * (detQ1 - detQ2), pixidx, num_of_pixels)
+    Umap, hits = binned_map(2 * (detU1 - detU2), pixidx, num_of_pixels)
 
-    healpy.write_map(map_filename, Imap)
+    healpy.write_map(map_filename, (Imap, Qmap, Umap, hits))
 
 
 if __name__ == '__main__':
